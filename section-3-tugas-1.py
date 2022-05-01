2e398c37737c01678b5099f23522b3f6219cadf8
ras24 <siregarassire@gmail.com>
2022-05-02 00:06:07 +0800

    update-tugas-1

--- section-3-tugas-1.py
+++ section-3-tugas-1.py
@@ -2,21 +2,18 @@ print('Ibu memberi perintah, "Beli 1 Botol Susu"')
 print('Anak menjawab, "OK"')
 print('Anak pergi ke toko')
 
-susu_ada = True
-telur_ada = True
+jumlah_susu = 5
+jumlah_telur = 3
 anak_pulang = "Anak pulang ke rumah"
 menyerahkan_hasil = "Menyerahkan hasil belanjanya kepada Ibu"
-if susu_ada == True:
+if jumlah_susu > 0:
     print('Anak mengecek harganya, cukup')
-    if telur_ada == True:
+    if jumlah_telur > 0:
         print('Anak membeli satu botol susu')
-        print(anak_pulang)
-        print(menyerahkan_hasil)
     else:
         print('Anak membeli 6 botol susu')
-        print(anak_pulang)
-        print(menyerahkan_hasil)
 else:
     print('Anak tidak jadi membeli susu')
-    print(anak_pulang)
-    print(menyerahkan_hasil)
+    
+print("Anak pulang ke rumah")
+print("Menyerahkan hasil belanjanya kepada Ibu")
